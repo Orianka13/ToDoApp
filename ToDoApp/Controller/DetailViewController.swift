5c305ab482a5442461122d429946f12497f82859
Orianka13 <odobrogor@gmail.com>
2022-03-10 10:30:32 +0300

    Тесты на ярлыки DetailVC

--- ToDoApp/Controller/DetailViewController.swift
+++ ToDoApp/Controller/DetailViewController.swift
@@ -14,9 +14,26 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
+    }
+    
+    var task: Task!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.titleLabel.text = self.task.title
+        self.descriptionLabel.text = self.task.description
+        
+        if let date = task.date {
+            self.dateLabel.text = self.dateFormatter.string(from: date)
+        }
+        
+        self.locationLabel.text = self.task.location?.name
     }
 }
--- ToDoAppTests/Controller/DetailViewControllerTests.swift
+++ ToDoAppTests/Controller/DetailViewControllerTests.swift
@@ -7,10 +7,12 @@
 
 import XCTest
 @testable import ToDoApp
+import CoreLocation
 
 class DetailViewControllerTests: XCTestCase {
     
     var controller: DetailViewController!
+    var task: Task!
     
     override func setUpWithError() throws {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -42,10 +44,60 @@ class DetailViewControllerTests: XCTestCase {
         
     }
     
+    func testHasLocationLabel() {
+        XCTAssertNotNil(self.controller.locationLabel)
+        XCTAssertTrue(self.controller.locationLabel.isDescendant(of: self.controller.view))
+        
+    }
+    
     //проверим есть ли MapKit на DetailViewController
     func testHasMapView() {
         XCTAssertNotNil(self.controller.mapView)
         XCTAssertTrue(self.controller.mapView.isDescendant(of: self.controller.view))
         
     }
+    
+    func setupTaskAndAppearanceTransition() {
+        
+        let coordinate = CLLocationCoordinate2D(latitude: 59.88304167, longitude: 30.38266797)
+        let location = Location(name: "Baz", coordinate: coordinate)
+        let date = Date(timeIntervalSince1970: 1646895570)
+        self.task = Task(title: "Foo", description: "Bar", date: date, location: location)
+        self.controller.task = self.task
+        
+        self.controller.beginAppearanceTransition(true, animated: true) //имитируем viewWillAppear
+        self.controller.endAppearanceTransition() // viewDidAppear
+        
+    }
+    
+    //проверим что наши ярлыки размещают в себе необходимую информацию по конкретному task
+    func testSettingTaskSetsTitleLabel(){
+        
+        self.setupTaskAndAppearanceTransition()
+        XCTAssertEqual(self.controller.titleLabel.text, "Foo")
+    }
+    
+    func testSettingTaskSetsDescriptionLabel(){
+        
+        self.setupTaskAndAppearanceTransition()
+        XCTAssertEqual(self.controller.descriptionLabel.text, "Bar")
+    }
+    
+    func testSettingTaskSetsDateLabel() {
+        
+        self.setupTaskAndAppearanceTransition()
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        if let date = self.task.date {
+            let stringDate = df.string(from: date)
+            XCTAssertEqual(self.controller.dateLabel.text, stringDate)
+        }
+    }
+    
+    func testSettingTaskSetsLocationNameLabel(){
+        
+        self.setupTaskAndAppearanceTransition()
+        XCTAssertEqual(self.controller.locationLabel.text, "Baz")
+    }
+    
 }
